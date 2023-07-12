c54750c126f5536d12e69c3cae2b8cf9ee8fc0af
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-07-12 13:37:40 +0100

    stack

--- Stack/Stack_Exercise.py
+++ Stack/Stack_Exercise.py
@@ -26,22 +26,47 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        new_element.next = self.head
+        self.head = new_element
 
     def delete_first(self):
-        "Delete the first (head) element in the LinkedList as return it"
-        pass
+        "Delete the first (head) element in the LinkedList and return it"
+        if self.head:
+            deleted_element = self.head
+            self.head = self.head.next
+            deleted_element.next = None
+            return deleted_element
+        else:
+            return None
 
 class Stack(object):
-    def __init__(self,top=None):
+    def __init__(self, top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
+        self.ll.insert_first(new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        pass
-    
- 
+        return self.ll.delete_first()
+
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a Stack
+stack = Stack(e1)
+
+# Test stack functionality
+stack.push(e2)
+stack.push(e3)
+print(stack.pop().value)  # Output: 3
+print(stack.pop().value)  # Output: 2
+print(stack.pop().value)  # Output: 1
+print(stack.pop())        # Output: None
+stack.push(e4)
+print(stack.pop().value)  # Output: 4
